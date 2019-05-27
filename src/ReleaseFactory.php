46a6d05d6234317f70a88c534b44129cc9123fcd
Tang Rufus <tangrufus@gmail.com>
2019-05-27 15:34:22 +0100

    Build to external git repo

--- src/ReleaseFactory.php
+++ src/ReleaseFactory.php
@@ -5,27 +5,19 @@ namespace Composer\Itineris\WordPress;
 
 use Composer\Itineris\WordPress\Requirement\RequirementCollection;
 use Composer\Itineris\WordPress\Util\Url;
-use Composer\Semver\VersionParser;
 use Composer\Util\RemoteFilesystem;
-use UnexpectedValueException;
 
 class ReleaseFactory
 {
-    protected const VERSION_PATTERN = '/\S+\/wordpress-(?<version>\S+[^IIS])(-IIS)?\.(zip|tar\.gz)/';
-
-    /** @var VersionParser */
-    protected $versionParser;
     /** @var RequirementCollection */
     protected $requirementCollection;
     /** @var RemoteFilesystem */
     protected $rfs;
 
     public function __construct(
-        VersionParser $versionParser,
         RequirementCollection $requirementCollection,
         RemoteFilesystem $rfs
     ) {
-        $this->versionParser = $versionParser;
         $this->requirementCollection = $requirementCollection;
         $this->rfs = $rfs;
     }
@@ -33,18 +25,15 @@ class ReleaseFactory
     public static function make(RemoteFilesystem $rfs): self
     {
         return new static(
-            new VersionParser(),
             RequirementCollection::make(),
             $rfs
         );
     }
 
-    public function build(string $downloadUrl): ?Release
+    public function build(string $version, string $downloadUrl): ?Release
     {
-        $version = $this->parseVersion($downloadUrl);
         $shasum = $this->getShasum($downloadUrl);
-
-        if (null === $version || null === $shasum) {
+        if (null === $shasum) {
             return null;
         }
 
@@ -59,25 +48,6 @@ class ReleaseFactory
             ],
             $this->requirementCollection->forWordPressCore($version)
         );
-    }
-
-    protected function parseVersion(string $url): ?string
-    {
-        preg_match(static::VERSION_PATTERN, $url, $matches);
-        $version = (string) ($matches['version'] ?? '');
-
-        return $this->isVersion($version) ? $version : null;
-    }
-
-    protected function isVersion(string $string): bool
-    {
-        try {
-            $this->versionParser->normalize($string);
-
-            return true;
-        } catch (UnexpectedValueException $exception) {
-            return false;
-        }
     }
 
     protected function getShasum(string $url): ?string
